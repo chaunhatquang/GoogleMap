f9f15fe359b8f699e277b3c455561b75c1e1e103
Nhat Quang <quangcn@msn.com>
2016-11-09 16:21:01 +0700

    done

---
+++ WriteNotes/app/src/main/java/notes/chaunhatquang/com/writenotes/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package notes.chaunhatquang.com.writenotes.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import notes.chaunhatquang.com.writenotes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
