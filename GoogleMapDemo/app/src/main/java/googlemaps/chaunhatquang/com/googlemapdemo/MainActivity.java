43e6e65d6b8f34a45a8697686adaac95986f8c6d
Nhat Quang <quangcn@msn.com>
2016-11-15 16:55:12 +0700

    code variable Main

--- GoogleMapDemo/app/src/main/java/googlemaps/chaunhatquang/com/googlemapdemo/MainActivity.java
+++ GoogleMapDemo/app/src/main/java/googlemaps/chaunhatquang/com/googlemapdemo/MainActivity.java
@@ -1,13 +1,43 @@
 package googlemaps.chaunhatquang.com.googlemapdemo;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 007;
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+    private SignInButton btnSignIn;
+    private Button btnSignOut,btnRevokeAccess;
+    private LinearLayout llProfile;
+    private ImageView imgProfilePic;
+    private TextView tvName,tvEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
